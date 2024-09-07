5ed0b2f31f566b09bfd0ba0042833878a6fad476
Iryna Ielkina <ielkinairinka@gmail.com>
2024-09-07 21:07:00 +0300

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StrictMode} from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
 
 import App from 'components/App';
@@ -173,8 +173,18 @@ console.log('component');
 
 //новый синтаксис react 18.1
 // const root = createRoot(document.getElementById('root')).render(<App />);
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
