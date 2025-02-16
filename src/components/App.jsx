54a4a8f43eb48f9f3715a6f361837782fbf2ffe8
Ielkina Iryna <ielkinairinka@gmail.com>
2025-02-16 18:54:09 +0300

    mod some files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,7 +39,7 @@ class App extends Component {
     return (
       <div className="container">
         <Header showModal={this.showModal} />
-        <Counter />
+        {/* <Counter /> */}
         {/* Убираем модалку  */}
         {/* {false && <Modal>Some</Modal>} */}
         {this.state.isShowModal && (
