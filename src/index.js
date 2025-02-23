8f3c5a5fa6a01ff067b7cf40a4bb1e1075f8fb49
Ielkina Iryna <ielkinairinka@gmail.com>
2025-02-23 20:06:56 +0300

    Add package-lock.json to .gitignore

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React, { Children } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; //делает ссылку с бэкэнда рабочей
+// import { BrowserRouter } from "react-router-dom"; //делает ссылку с бэкэнда рабочей
 import App from "./components/App";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-    // <React.StrictMode>
-  <BrowserRouter>
+  <React.StrictMode>
+    {/* <BrowserRouter> */}
     <App />
-  </BrowserRouter>
-  // </React.StrictMode>
+    {/* </BrowserRouter> */}
+  </React.StrictMode>
 );
 
 // https://youtu.be/TwWtjt07oik?t=3004
