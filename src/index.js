3ac73f1bfb0a6d0b6fae53e5462a9b02d1c45e27
Ielkina Iryna <ielkinairinka@gmail.com>
2025-02-23 16:57:19 +0300

    modified index.{html,js}

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React, { Children } from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; //делает ссылку с бэкэнда рабочей
 import App from "./components/App";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+    // <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
+
+// https://youtu.be/TwWtjt07oik?t=3004
