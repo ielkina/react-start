70ec6cb28183c3fd69b5603a8a6cc5c0a3d28393
Iryna Ielkina <40836100+ielkina@users.noreply.github.com>
2025-03-07 12:12:57 +0300

    update config files

--- config/craco.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  devServer: (devServerConfig) => {
-    devServerConfig.setupMiddlewares = (middlewares, devServer) => {
-      // ваш код
-      return middlewares;
-    };
-    return devServerConfig;
-  },
-};
